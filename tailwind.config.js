864a21deba2343c53bbb80d0432aeeb095499bf2
fujita_kohei <fujita.kohei04@gmail.com>
2026-02-19 21:53:53 +0900

    fix: tailwind setup adapt to files in src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // ← ここで src 以下の全 tsx を指定しているか？
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
